82f8afbd94ad45769490936a81b40d7a920c5b1f
Caleb Xiang <caleb.xiangkh@shopee.com>
2023-10-30 13:28:20 +0800

    k6 select

---
+++ k6/price.js
@@ -0,0 +1,10 @@
+import http from "k6/http";
+
+export const options = {
+    vus: 30,
+    duration: "30s",
+};
+
+export default function () {
+    http.get("http://127.0.0.1:3000/random-by-price");
+}
