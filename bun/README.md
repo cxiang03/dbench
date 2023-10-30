fd4961906581ba2c75a3ed03b7995ddebf0a479e
Caleb Xiang <caleb.xiangkh@shopee.com>
2023-10-30 11:38:21 +0800

    update linter

--- bun/README.md
+++ bun/README.md
@@ -1,4 +1,4 @@
-# bbench
+# dbench
 
 To install dependencies:
 
--- bun/index.ts
+++ bun/index.ts
@@ -5,30 +5,30 @@ const dsn = Bun.env.DSN ?? "mysql://root:password@127.0.0.1:3306/dbench";
 const sequelize = new Sequelize(dsn);
 
 try {
-  console.log("connecting to the database...");
-  await sequelize.authenticate();
-  console.log("connection has been established successfully");
+    console.log("connecting to the database...");
+    await sequelize.authenticate();
+    console.log("connection has been established successfully");
 } catch (error) {
-  console.error("unable to connect to the database:", error);
+    console.error("unable to connect to the database:", error);
 }
 
 const server = Bun.serve({
-  async fetch(req) {
-    const url = new URL(req.url);
-    switch (url.pathname) {
-      case "/":
-        const rst = await sequelize.query("SELECT NOW()", { type: QueryTypes.SELECT });
-        if (rst.length === 0) {
-          return new Response();
+    async fetch(req) {
+        const url = new URL(req.url);
+        switch (url.pathname) {
+            case "/":
+                const rst = await sequelize.query("SELECT NOW()", { type: QueryTypes.SELECT });
+                if (rst.length === 0) {
+                    return new Response();
+                }
+                const now = rst[0];
+                const body = figlet.textSync("bun!");
+                return new Response(body + "\n" + JSON.stringify(now, null, 2));
+            default:
+                return new Response();
         }
-        const now = rst[0];
-        const body = figlet.textSync("bun!");
-        return new Response(body + "\n" + JSON.stringify(now, null, 2));
-      default:
-        return new Response();
-    }
-  },
-  port: 3000,
+    },
+    port: 3000,
 });
 
 console.log(`listening on http://localhost:${server.port}`);
